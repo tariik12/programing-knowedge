bdedf10bc7c8fb70e756ff440c82b22a61679cbb
tarikul-islam <tariikul12@gmail.com>
2023-03-03 00:35:44 +0600

    All dynamic img show Done

--- index.js
+++ index.js
@@ -1,28 +1,53 @@
 const dataLoad = ()=>{
     fetch('https://openapi.programming-hero.com/api/ai/tools')
     .then(res =>res.json())
-    .then(data =>displayData(data.data))
+    .then(data =>displayData(data.data.tools.slice(0,6)))
+    .catch(err =>{
+        console.log(err)
+    })
+}
+
+const displayData =(data) =>{
+    document.getElementById('show-all-data').classList.remove('d-none');
+    const displayContainer = document.getElementById('display-container');
+    displayContainer.innerHTML = '';
+    data.forEach(singleData =>{
+        const {name,features,image,published_in} =singleData;
+        
+        const createDiv =document.createElement('div');
+        createDiv.innerHTML =`
+        
+        <div class="col h-100">
+        <div class="card h-100 p-2">
+        <img src="${image}" class="card-img-top  rounded h-100 "  alt="...">
+        <div class="h-100 card-body">
+        <h5 class="card-title">Features</h5>
+        <p class="card-text">${features}</p>
+      <hr class="container">
+      <h5>${name}</h5>
+      <div class="d-flex ">
+      <p class="card-text">${published_in}</p>
+      <button class="btn btn-outline ms-auto" data-bs-toggle="modal" data-bs-target="#exampleModal"><img src="./image/btn.png" class=" img-fluid " alt=""></button>
+      </div>    
+      </div>
+      </div>
+      </div>    
+      `
+      displayContainer.appendChild(createDiv);
+    })
+    
 }
 dataLoad()
-const displayData =(data) =>{
-    // console.log(data.tools)
-data.tools.forEach(singleData =>{
-    console.log(singleData.name)
-    const {name} =singleData;
-    const displayContainer = document.getElementById('display-container');
-    const createDiv =document.createElement('div');
-    createDiv.innerHTML =`
+
+showAll =() =>{
+  
+    document.getElementById('show-all-data').classList.add('d-none');
+    fetch('https://openapi.programming-hero.com/api/ai/tools')
+    .then(res =>res.json())
+    .then(data =>displayData(data.data.tools))
+    .catch(err =>{
+        console.log(err)
+    })
+   
     
-                <div class="col">
-                  <div class="card h-100">
-                   <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">${name}</h5>
-                      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    </div>
-                </div>
-    
-    `
-    displayContainer.appendChild(createDiv);
-})
-}
+  }
