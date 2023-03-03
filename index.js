dc9bf69bc020f24bf8d93e8ab2b6056f86d758c6
tarikul-islam <tariikul12@gmail.com>
2023-03-03 16:28:11 +0600

    Spiiner add Done

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const dataLoad = ()=>{
     .catch(err =>{
         console.log(err)
     })
+    toggleSpinner(true);
 }
+/* show six Data */
 const displayData =(data) =>{
+    toggleSpinner(false);
     document.getElementById('show-all-data').classList.remove('d-none');
     const displayContainer = document.getElementById('display-container');
     displayContainer.innerHTML = '';
@@ -35,7 +38,7 @@ const displayData =(data) =>{
     })
     
 }
-dataLoad()
+/* show All Data */
 showAll =() =>{
     document.getElementById('show-all-data').classList.add('d-none');
     fetch('https://openapi.programming-hero.com/api/ai/tools')
@@ -44,78 +47,80 @@ showAll =() =>{
     .catch(err =>{
         console.log(err)
     })
-  }
- const  loadSingleDataDetails =(id) =>{
-
+}
+/* spinner */
+const toggleSpinner = isLoading =>{
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+      loaderSection.classList.remove('d-none')
+    }
+    else{
+        loaderSection.classList.add('d-none');
+    }
+}
+/* load Single Data */
+const  loadSingleDataDetails =(id) =>{
+    
     const URL =`https://openapi.programming-hero.com/api/ai/tool/${id}`
     fetch(URL)
     .then(res =>res.json())
     .then(data =>showSingleDataDetails(data))
-    // .catch(err =>{
-    //     console.log(err)
-    // })
-
-  }
-  const  showSingleDataDetails =(data =>{
-//    console.log(data.data.pricing[0].price)
-//         console.log(data.data.pricing[0].plan)
-//         console.log(data.data.pricing[1].price)
-//         console.log(data.data.pricing[1].plan)
-//         console.log(data.data.pricing[2].price)
-//         console.log(data.data.pricing[2].plan)
-
-
- console.log(data.data.input_output_examples[0].input)
-        // console.log(data.data.features[2].feature_name)
-        // console.log(data.data.features[3].feature_name)
-
-    const showSingleData = document.getElementById('show-single-data');
-    const {description} = data.data;
-    const createDiv = document.createElement('div');
-    createDiv.innerHTML = `
-    <div   class="row">
-    <div  class="card col-md-6">
-    <h4>${description}</h4>
-    <div class="d-flex justify-content-between gap-2">
-    <div class="card p-2">
-      <p class="card-text"><span>${data.data.pricing[0].price}</span>   <span>${data.data.pricing[0].plan}</span></p>
-    </div>
-    <div class="card p-2">
+        toggleSpinner(true);
+    }
+/* show Single Data */
+    const  showSingleDataDetails =(data =>{
+        toggleSpinner(false);
+        // console.log(data.data.input_output_examples[0].input)
+        const showSingleData = document.getElementById('show-single-data');
+        showSingleData.innerHTML = '';
+        // const {description} = data.data;
+        const createDiv = document.createElement('div');
+        createDiv.innerHTML = `
+        <div   class="row">
+        <div  class="card col-md-6">
+        <h4>${data.data.description}</h4>
+        <div class="d-flex justify-content-between gap-2">
+        <div class="card p-2">
+        <p class="card-text"><span>${data.data.pricing[0].price}</span>   <span>${data.data.pricing[0].plan}</span></p>
+        </div>
+        <div class="card p-2">
       <p class="card-text"><span>${data.data.pricing[1].price}</span>   <span>${data.data.pricing[1].plan}</p>
-    </div>
-    <div class="card p-2">
+      </div>
+      <div class="card p-2">
       <p class="card-text"><span>${data.data.pricing[2].price}</span>   <span>${data.data.pricing[2].plan}</p>
-    </div>
-  </div>
-  <div class="d-flex justify-content-between my-2  gap-4">
-  <div >
-    <h4>Feature</h4>
+      </div>
+      </div>
+      <div class="d-flex justify-content-between my-2  gap-4">
+      <div >
+      <h4>Feature</h4>
     <ul>
-        <li>${data.data.features[1].feature_name}</li>
-        <li>${data.data.features[2].feature_name}</li>
-        <li>${data.data.features[3].feature_name}</li>
-  </ul>
-  </div>
-  <div class="pe-0 me-0 ms-auto" >
+    <li>${data.data.features[1].feature_name}</li>
+    <li>${data.data.features[2].feature_name}</li>
+    <li>${data.data.features[3].feature_name}</li>
+    </ul>
+    </div>
+    <div class="pe-0 me-0 ms-auto" >
     <h4 class="me-2">integration</h4>
     <ul>
-        <li>${data.data.integrations[0]}</li>
-        <li>${data.data.integrations[1]}</li>
-        <li>${data.data.integrations[2]}</li>
+    <li>${data.data.integrations[0]}</li>
+    <li>${data.data.integrations[1]}</li>
+    <li>${data.data.integrations[2]}</li>
     </ul>
-  </div>
- </div>
+    </div>
+    </div>
     </div>
     <div class="card col-md-6 ms-auto">
     <div class="p-3  position-relative">
-            <img src="${data.data.image_link[0]}" class="card-img-top rounded " alt="...">
-            <p class="btn btn-danger p-0 m-3 w-50  position-absolute top-0  start-50 translate-middle" ><span>${data.data.accuracy.score}</span>% accuracy</p>
-        </div>
-        <div class="text-center">
+    <img src="${data.data.image_link[0]}" class="card-img-top rounded " alt="...">
+    <p class="btn btn-danger p-0 m-3 w-50  position-absolute top-0  start-50 translate-middle" ><span>${data.data.accuracy.score}</span>% accuracy</p>
+    </div>
+    <div class="text-center">
             <h5 class="mt-3">${data.data.input_output_examples[0].input}</h5>
             <p class="mt-3">${data.data.input_output_examples[0].output}</p>
-        </div>
-  </div>
-    `
-showSingleData.appendChild(createDiv)
-    })
+            </div>
+            </div>
+            `
+            showSingleData.appendChild(createDiv)
+        })
+        
+        dataLoad()
